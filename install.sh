419b85cfbad64207061da08aa715344468c09eb2
Borys Kondrashov <95252258+Tuburni@users.noreply.github.com>
2023-05-22 22:42:21 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------------------------
+#   This script will install Terraform and Ansible to this machine
+#   To run this scirpt write in terminal "bash install.sh"
+# ------------------------------------------------------------------------------------------------
+
+echo ---------------------------------------------------------------------------------------------
+echo    This script will install Terraform and Ansible to this machine
+echo ---------------------------------------------------------------------------------------------
+
+
+sudo apt-get update -y
+wget https://releases.hashicorp.com/terraform/1.2.5/terraform_1.2.5_linux_amd64.zip
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt install ansible
+sudo apt install zip -y
+sudo unzip terraform_1.2.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform version
+
+echo ---------------------------------------------------------------------------------------------
+echo    Congradulation, Terraform and Ansible was sucesfully instaled
+echo ---------------------------------------------------------------------------------------------
